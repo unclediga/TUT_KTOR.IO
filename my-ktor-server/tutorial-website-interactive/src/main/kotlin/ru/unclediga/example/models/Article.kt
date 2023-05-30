0eb4d1acdae23ba23dcbc8adc55e4c85ac9e37af
worker <worker@firm.ru>
2023-05-30 13:02:22 +0300

    Refactor a template

--- my-ktor-server/tutorial-website-interactive/src/main/kotlin/ru/unclediga/example/models/Article.kt
+++ my-ktor-server/tutorial-website-interactive/src/main/kotlin/ru/unclediga/example/models/Article.kt
@@ -12,7 +12,11 @@ private constructor(val id: Int, var title: String, var body: String) {
 }
 
 
-val articles = mutableListOf(Article.newEntry(
-    "The drive to develop!",
-    "...it's what keeps me going."
-))
+val articles = mutableListOf(
+    Article.newEntry(
+        "The drive to develop!",
+        "...it's what keeps me going."
+    ),
+    Article.newEntry("My article 1", "Java super!"),
+    Article.newEntry("My article 2", "Kotlin is not bad!")
+)
