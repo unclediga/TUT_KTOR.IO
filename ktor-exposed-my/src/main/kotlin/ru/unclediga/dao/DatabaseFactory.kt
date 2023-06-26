7a47eedae2d838c618505ab077048668bc8e2d1f
worker <worker@firm.ru>
2023-06-26 15:18:38 +0300

    Enable connection pooling

--- ktor-exposed-my/src/main/kotlin/ru/unclediga/dao/DatabaseFactory.kt
+++ ktor-exposed-my/src/main/kotlin/ru/unclediga/dao/DatabaseFactory.kt
@@ -9,17 +9,20 @@ import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransacti
 import java.io.File
 import io.ktor.server.config.ApplicationConfig
 
+import com.zaxxer.hikari.*
 // import org.jetbrains.exposed.sql.transactions.experimental.*;
 // import kotlinx.coroutines.*;
 
 object DatabaseFactory {
+
     fun init(config: ApplicationConfig) {
         val driverClassName = config.property("storage.driverClassName").getString()
-        val jdbcUrl = config.property("storage.jdbcUrl").getString()  + 
-        config.propertyOrNull("storage.dbFilePath")?.getString()?.let {
-          File(it).canonicalFile.absolutePath
-        } ?: ""  
-        val database = Database.connect(jdbcUrl, driverClassName)
+        val jdbcUrl = config.property("storage.jdbcUrl").getString() +
+            config.propertyOrNull("storage.dbFilePath")?.getString()?.let {
+          	File(it).canonicalFile.absolutePath
+            } ?: ""  
+
+        val database = Database.connect(createHikariDataSource(url = jdbcUrl, driver = driverClassName))
 
         transaction(database) {
             SchemaUtils.create(Articles)
@@ -28,6 +31,19 @@ object DatabaseFactory {
 
     suspend fun <T> dbQuery(block: suspend () -> T): T =
         newSuspendedTransaction(Dispatchers.IO) { block() }
+
+   private fun createHikariDataSource(url: String, driver: String) = HikariDataSource(
+   	HikariConfig().apply {
+		driverClassName = driver
+	        jdbcUrl = url
+        	maximumPoolSize = 3
+	        isAutoCommit = false
+	        transactionIsolation = "TRANSACTION_REPEATABLE_READ"
+	        validate()
+	})   
+      
+   
+    
 }
 
 
